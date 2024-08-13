283df360e4762c2bd61d8d54614b01ed76fb0c75
Prof-Ursi <8743@holbertonstudents.com>
2024-08-13 15:13:57 +0200

    Updating 1-concat.py, working & pycodestyle approved

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Module with a string concatenation function. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Description:
+        Concatenate two strings.
+
+    Args:
+        - str1 (str): first string to concatenate.
+        - str2 (str): second string to concatenate.
+
+    Returns:
+        (str): the concatenated string of the two input strings.
+    """
+    return (str1 + str2)
