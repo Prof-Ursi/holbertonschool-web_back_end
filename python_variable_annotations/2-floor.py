fe53a67d2ca045900be9e9c51e20704e0cc15b04
Prof-Ursi <8743@holbertonstudents.com>
2024-08-13 15:21:05 +0200

    Updating 2-floor.py, working & pycodestyle approved

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module with a floor conversion function. """
+
+
+def floor(n: float) -> int:
+    """
+    Description:
+        Converts a float to its floor value as an integer.
+
+    Args:
+        n (float): the floating-point number to convert.
+
+    Returns:
+        (int): the floor value of the input float `n` as an integer.
+    """
+    return (int(n))
