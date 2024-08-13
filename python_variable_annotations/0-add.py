0102389756873ba14650a54541fdee0af849f96d
Prof-Ursi <8743@holbertonstudents.com>
2024-08-13 15:10:39 +0200

    Updating 0-add.py, working & pycodestyle approved

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Module with a float addition function. """
+
+
+def add(a: float, b: float) -> float:
+    """
+    Description:
+        Add two floating-point numbers.
+
+    Args:
+        - a (float): first input.
+        - b (float): second input.
+
+    Returns:
+        (float): the sum of the two input numbers.
+    """
+    return (a + b)
