ebd7f4cfc873c260387d714a67778d270ad6ab5a
Prof-Ursi <8743@holbertonstudents.com>
2024-08-14 17:47:18 +0200

    Updating 2-measure_runtime.py, working & pycodestyle approved

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module with a function measuring the average
+execution time of the "wait_n()" routine.
+"""
+
+from asyncio import run
+from time import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the total execution time for "wait_n(n, max_delay)"
+    and return the average execution time by dividing
+    the total execution time by n.
+
+    Args:
+        - n (int): the number of times to execute "wait_n()" concurrently.
+        - max_delay (int): the maximum delay in seconds for
+        each "wait_n()" execution.
+
+    Returns:
+    - (float): the average execution time per "wait_n()" call.
+    """
+    start_time = time()
+    run(wait_n(n, max_delay))
+    end_time = time()
+    total_time = end_time - start_time
+
+    return float(total_time / n)
