38e47487851280df6985f532568089456c65e42d
Prof-Ursi <8743@holbertonstudents.com>
2024-09-20 20:56:16 +0200

    Fixing errors

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,30 +1,38 @@
 const http = require('http');
+const url = require('url');
+
 const countStudents = require('./3-read_file_async');
 
-const HOSTNAME = '127.0.0.1';
-const PORT = 1245;
-const DATABASE = process.argv[2];
+const path = process.argv[2];
 
-const app = http.createServer(async (req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+const app = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/') {
+    res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 200;
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
+  } else if (parsedUrl.pathname === '/students') {
+    res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 200;
+
     res.write('This is the list of our students\n');
-    try {
-      const { students, csStudents, sweStudents } = await countStudents(DATABASE);
-      res.write(`Number of students: ${students.length}\n`);
-      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-      res.end();
-    } catch (error) {
-      res.end(error.message);
-    }
+
+    countStudents(path)
+      .then((output) => {
+        res.end(output);
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
   } else {
-    res.writeHead(404);
-    res.end('Invalid request');
+    res.statusCode = 404;
+    res.end('Not Found');
   }
 });
 
-app.listen(PORT, HOSTNAME);
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
 module.exports = app;
